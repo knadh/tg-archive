b718e62e1fe0b13e60f381b42c5c2c36a21fb74f
A Farzat <a@farzat.xyz>
2021-12-17 15:01:09 +0900

    Symlink files and directories when building
    This is preferred as copying takes much more time and uses up space.

--- tgarchive/build.py
+++ tgarchive/build.py
@@ -85,8 +85,7 @@ class Build:
 
         # The last page chronologically is the latest page. Make it index.
         if fname:
-            shutil.copy(os.path.join(self.config["publish_dir"], fname),
-                        os.path.join(self.config["publish_dir"], "index.html"))
+            os.symlink(fname, os.path.join(self.config["publish_dir"], "index.html"))
 
         # Generate RSS feeds.
         if self.config["publish_rss_feed"]:
@@ -166,13 +165,10 @@ class Build:
         # Copy the static directory into the output directory.
         for f in [self.config["static_dir"]]:
             target = os.path.join(pubdir, f)
-            if os.path.isfile(f):
-                shutil.copyfile(f, target)
-            else:
-                shutil.copytree(f, target)
+            os.symlink(os.path.abspath(f), target)
 
-        # If media downloading is enabled, copy the media directory.
+        # If media downloading is enabled, symlink the media directory.
         mediadir = self.config["media_dir"]
         if os.path.exists(mediadir):
-            shutil.copytree(mediadir, os.path.join(
+            os.symlink(os.path.abspath(mediadir), os.path.join(
                 pubdir, os.path.basename(mediadir)))

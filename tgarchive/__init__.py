5d6a73d80b168d7fcccb24a3efaa39c9050bfe69
Kailash Nadh <kailash@nadh.in>
2022-02-13 14:21:44 +0530

    Add ability to filter media downloads by MIME type.
    A new config key `media_mime_types: []` in the config specificies
    the MIME type of files to download. If it's left empty, all files are
    downloaded.
    
    Closes #49.

--- tgarchive/__init__.py
+++ tgarchive/__init__.py
@@ -20,6 +20,7 @@ _CONFIG = {
     "avatar_size": [64, 64],
     "download_media": False,
     "media_dir": "media",
+    "media_mime_types": [],
     "fetch_batch_size": 2000,
     "fetch_wait": 5,
     "fetch_limit": 0,
